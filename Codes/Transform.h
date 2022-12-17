46a3e30b12c6e91f4904dc8ac9b2db298c57fe7e
Kadir Sonmezisik <101130181+akadirson@users.noreply.github.com>
2022-12-17 23:22:17 +0300

    Update Transform.h

--- Codes/Transform.h
+++ Codes/Transform.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "PointCloud.h"
 
-class Transform:public PointCloud{
+class Transform :public PointCloud {
 private:
 	double angles[3];
 	double trans[3];
 	double transMatrix[4][4];
 public:
 	Transform();
-	void setAngles();
-	double getAngles();
-	void setTrans();
-	double getTrans();
+	void setAngles(double, double, double);
+	double* getAngles();
+	void setTrans(double, double, double);
+	double* getTrans();
 	void setRotation(double angles[]);
 	void setTranslation(double trans[]);
 	Point doTransform(Point p);
