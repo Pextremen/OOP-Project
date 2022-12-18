fad67ea4b062097502cdbebc4cb420b825482120
Kadir Sonmezisik <101130181+akadirson@users.noreply.github.com>
2022-12-18 18:36:23 +0300

    New verision PointCloud.cpp

--- Codes/PointCloud.cpp
+++ Codes/PointCloud.cpp
@@ -1,30 +1,55 @@
 #include "PointCloud.h"
 
-PointCloud::PointCloud(int pointNumber) {
-	this->pointNumber = pointNumber;
-	points = new Point[pointNumber];
+PointCloud::PointCloud(int pointNumber){ //CONSTRUCTOR
+	if (pointNumber >= 0) { //Dizi size negatif olmamali
+		this->pointNumber = pointNumber;
+		points = new Point[pointNumber];
+	}
+	else {
+		this->pointNumber = 0;
+		points = new Point[pointNumber];
+	}
 }
 
-PointCloud::PointCloud(PointCloud& p) {
-	
-}
-
-void PointCloud::setpointNumber(int pointNumber) {
-	this->pointNumber = pointNumber;
-}
-
-int PointCloud::getpointNumber() { return this->pointNumber; }
-
-PointCloud* PointCloud::operator+(const PointCloud& p) {
-
-	return nullptr;
-}
-
-void PointCloud::operator=(const PointCloud& p) {
-	this->points = p.points;
+PointCloud::PointCloud(PointCloud& p){ // Copy constructor
 	this->pointNumber = p.pointNumber;
+	points = new Point[pointNumber];
+	for (int i = 0; i < p.pointNumber; i++) {
+		points[i] = p.points[i];
+	}
 }
-Point* PointCloud::getPoints()
-{
-	return points;
+
+void PointCloud::setpointNumber(int newpointNumber){
+	Point* temp = new Point[newpointNumber];
+	for (int i = 0; i < newpointNumber; i++) {
+		temp[i] = points[i];
+	}
+	this->pointNumber = newpointNumber;
+	delete[] points;
+	points = temp; //ikinci for ile tek tek temp ten points'e atmaya gerek var mı
+}
+
+int PointCloud::getpointNumber(){return this->pointNumber;}
+Point* PointCloud::getPoints() {return points;}
+
+PointCloud PointCloud::operator+(const PointCloud& p){
+	PointCloud temp;
+	int tempsize = p.pointNumber + this->pointNumber;
+	for (int i = 0; i < tempsize; i++) {
+		if (i < p.pointNumber) {
+			temp.points[i] = p.points[i];
+		}
+		if (i >= p.pointNumber) {
+			temp.points[i] = this->points[i - p.pointNumber];
+		}
+	}
+	return temp;
+}
+
+void PointCloud::operator=(const PointCloud& p){
+	this->pointNumber = p.pointNumber;
+	points = new Point[pointNumber];
+	for (int i = 0; i < p.pointNumber; i++) {
+		points[i] = p.points[i];
+	}
 }
