51147af2c210ba5035c5a795f2291e90c2a584d9
SAMET SOYSAL <sametsoysal_33@hotmail.com>
2022-12-23 19:36:20 +0300

    update test cpp's

--- OOP Project/OOP Project/testmain.cpp
+++ OOP Project/OOP Project/testmain.cpp
@@ -6,16 +6,7 @@
 #include "PointCloudRecorder.h"
 #include "Transform.h"
 int main() {
-	ifstream cam1;
-	string word;
-	string line;
-	string line2;
-	cam1.open("cam1.txt");
-	getline(cam1, line);
-	getline(cam1, line);
-	int a = stod(line);
-	int j = 0;
-	cout << a << endl;
+	
 
 
 
