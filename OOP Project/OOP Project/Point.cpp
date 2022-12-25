3f028b0e3845a7e9ce885e30bc310bff4393f074
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2022-12-25 13:08:26 +0300

    Update Point.cpp

--- OOP Project/OOP Project/Point.cpp
+++ OOP Project/OOP Project/Point.cpp
@@ -2,14 +2,17 @@
 
 Point::Point(double x, double y, double z):x(x),y(y),z(z){}
 double Point::getX(){
+	/// Returns each x values of coordinates
 	return this->x;
 }
 
 double Point::getY(){
+	/// Returns each y values of coordinates
 	return this->y;
 }
 
 double Point::getZ(){
+	/// Returns each z values of coordinates
 	return this->z;
 }
 void Point::setX(double x) { this->x = x; }
@@ -18,6 +21,7 @@ void Point::setZ(double z) { this->z = z; }
 
 
 double Point::distance(const Point& p) const{
+	 /// Calculates the euclidean distance between two points, initial point-point with parameter
 	double distance = pow((this->x - p.x), 2) + pow((this->y - p.y), 2) + pow((this->z - p.z), 2);
 	return pow(distance, 0.5);
 }
