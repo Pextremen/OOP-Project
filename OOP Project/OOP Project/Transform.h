890af6b2e8db874fe57c9be9b35ef4de7d3f8de5
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2022-12-25 13:24:44 +0300

    Update Transform.h

--- OOP Project/OOP Project/Transform.h
+++ OOP Project/OOP Project/Transform.h
@@ -3,12 +3,13 @@
 
 class Transform :public PointCloud {
 private:
-	double angles[3];
-	double trans[3];
-	double transMatrix[4][4];
-	double rotationMatrix[3][3];
+	double angles[3];  /*!<Holds three angle values, these angles will be used in rotation.*/
+	double trans[3]; /*!<Holds the distances between the origins of two coordinate axes.*/  
+	double transMatrix[4][4]; /*!<Creates the transformation matrix*/ 
+	double rotationMatrix[3][3]; /*!<Creates the rotation matrix*/ 
 
 public:
+	//! Constructor of Transform Class
 	Transform();
 	void setAngles(double, double, double);
 	double* getAngles();
@@ -18,5 +19,5 @@ public:
 	void setRotation(double rotation[3][3]);
 	void setTranslation();
 	Point doTransform(Point p);
-	PointCloud doTransform(PointCloud &pc);
+	PointCloud doTransform(PointCloud& pc);
 };
