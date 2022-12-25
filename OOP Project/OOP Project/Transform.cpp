b4d7324ad212551d2152ab1493864fc22d8e6237
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2022-12-25 13:33:47 +0300

    Update Transform.cpp

--- OOP Project/OOP Project/Transform.cpp
+++ OOP Project/OOP Project/Transform.cpp
@@ -23,6 +23,7 @@ Transform::Transform(){
 
 void Transform::setAngles(double a, double b, double c)
 {
+	///Sets angles
 	angles[0] = a;
 	angles[1] = b;
 	angles[2] = c;
@@ -30,6 +31,7 @@ void Transform::setAngles(double a, double b, double c)
 
 double* Transform::getAngles()
 {
+	///Returns angles
 	return angles;
 }
 
@@ -44,7 +46,8 @@ double* Transform::getTrans()
 	return trans;
 }
 
-void Transform::setRotation(double ang[3]){ //açý verilmiþse
+void Transform::setRotation(double ang[3]){ 
+	///Creates the rotationMatrix by performing the rotation operation
 	//alfa = angles[2] || beta = angles[1] || gama = angles[0] 
 	rotationMatrix[0][0] = cos(ang[2]) * cos(ang[1]);
 	rotationMatrix[0][1] = (cos(ang[2]) * sin(ang[1]) * sin(ang[0])) - (sin(ang[2]) * cos(ang[0]));
@@ -57,7 +60,8 @@ void Transform::setRotation(double ang[3]){ //açý verilmiþse
 	rotationMatrix[2][2] = cos(ang[1]) * cos(ang[0]);
 
 }
-void Transform::setRotation(double rotation[3][3]) { //direkt matris verilmiþse
+void Transform::setRotation(double rotation[3][3]) { 
+	///Creates the rotationMatrix
 	rotationMatrix[0][0] = rotation[0][0];
 	rotationMatrix[0][1] = rotation[0][1];
 	rotationMatrix[0][2] = rotation[0][2];
@@ -70,17 +74,17 @@ void Transform::setRotation(double rotation[3][3]) { //direkt matris verilmiþse
 }
 void Transform::setTranslation()
 {
-	//transMatrix, rotationMatrix ve trans'ýn býrlesiminden olusur:
+	//transMatrix is a combination of rotationMatrix and trans
 	//R R R T 
 	//R R R T
 	//R R R T
 	//0 0 0 1
-	for (int i = 0; i < 3; i++){//aktarma 1
+	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
 			transMatrix[i][j] = rotationMatrix[i][j];
 		}
 	}
-	for (int i = 0; i < 3; i++) { //aktarma 2
+	for (int i = 0; i < 3; i++) {
 		transMatrix[i][3] = trans[i];
 	}
 	// aktarma 3 son satir
@@ -91,17 +95,18 @@ void Transform::setTranslation()
 }
 
 Point Transform::doTransform(Point p){
+	///Converts received points. 
 	//old: X Y Z 1
 	double oldPoint[4] = { p.getX(),p.getY(),p.getZ(),1 };
 	//new: X Y Z 1 
 	double newPoint[4] = { 0 };
-	//matris çarpýmý
+	//matris Ã§arpÃ½mÃ½
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			newPoint[i] += (transMatrix[i][j] * oldPoint[j]);
 		}
 	}
-	//elde edilen yeni matris yeni noktaya aktarýlacak
+	//The new matrix obtained will be transferred to the new point
 	Point originPoint;
 	originPoint.setX(newPoint[0]);
 	originPoint.setY(newPoint[1]);
@@ -110,7 +115,8 @@ Point Transform::doTransform(Point p){
 }
 
 PointCloud Transform::doTransform(PointCloud &pc){
-	PointCloud newPointCloud(pc.getpointNumber()); //ayni boyutta empty PointCloud
+	///Converts the imported point cloud.
+	PointCloud newPointCloud(pc.getpointNumber()); ///Creates same size empty PointCloud
 	for (int i = 0; i < pc.getpointNumber(); i++) {
 		newPointCloud.getPoints()[i] = doTransform(pc.getPoints()[i]);
 	}
