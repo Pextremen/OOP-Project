08f0152994dd5ee20c5ee870eb779e33030a86ca
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2022-12-25 13:12:09 +0300

    Update Point.h

--- OOP Project/OOP Project/Point.h
+++ OOP Project/OOP Project/Point.h
@@ -6,7 +6,7 @@ class Point {
 private:
 	double x; /*!< x variable represents the (1.) x coordinate of the points */
 	double y; /*!< y variable represents the (2.) y coordinate of the points */
-	double z; /*!< z variable represent the (3.) z coordinate of the points */
+	double z; /*!< z variable represents the (3.) z coordinate of the points */
 public:
 	//! Constructor of Point Class
 	Point(double x = 0, double y = 0, double z = 0);
