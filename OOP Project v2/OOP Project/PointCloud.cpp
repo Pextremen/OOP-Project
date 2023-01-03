a961cfdc91cf8c4606967f121f868b5f692bd6ac
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2023-01-03 21:02:33 +0300

    Update PointCloud.cpp
    using vector

--- OOP Project v2/OOP Project/PointCloud.cpp
+++ OOP Project v2/OOP Project/PointCloud.cpp
@@ -1,74 +1,48 @@
+///using vector
 #include "PointCloud.h"
+using namespace std;
 
-PointCloud::PointCloud(int pointNumber) { 
+PointCloud::PointCloud(int pointNumber) {
 	///constructor
-	if (pointNumber >= 0) { //Dizi size negatif olmamali
-		this->pointNumber = pointNumber;
-		points = new Point[pointNumber];
-	}
-	else {
-		this->pointNumber = 0;
-		points = new Point[pointNumber];
-	}
+	this->points.resize(pointNumber);  //resize() changes the size of vector
 }
 
-PointCloud::PointCloud(PointCloud& p) { 
+PointCloud::PointCloud(PointCloud& p) {
 	/// Copy constructor
-	this->pointNumber = p.pointNumber;
-	points = new Point[pointNumber];
-	for (int i = 0; i < p.pointNumber; i++) {
-		points[i] = p.points[i];
-	}
+	this->points = p.points;
 }
+
 void PointCloud::setpointNumber(int newpointNumber)
 {
-	///set size of array
-	Point* tmppoints = new Point[newpointNumber];
-	for (int i = 0; i < pointNumber; i++)
-	{
-		tmppoints[i] = points[i];
-	}
-	delete[] points;
-
-	int oldPointNumber = pointNumber;
-	pointNumber = newpointNumber;
-	points = new Point[pointNumber];
-	for (int i = 0; i < oldPointNumber; i++)
-	{
-		points[i] = tmppoints[i];
-	}
-	delete[] tmppoints;
+	this->points.resize(newpointNumber);
 }
-int PointCloud::getpointNumber() { 
-	/// return pointNumber(size of array)
-	return this->pointNumber; }
-Point* PointCloud::getPoints() {
-	/// return array
-	return points; }  
-void PointCloud::setPoints(int i, Point p) { 
-	/// assign value to array
-	this->points[i] = p; }
+
+int PointCloud::getpointNumber() {
+	/// return pointNumber(size of vector) 
+	return this->points.size();
+}
+
+vector<Point>& PointCloud::getPoints() {   
+	/// returns vector
+	return this->points;
+}
+
+void PointCloud::setPoints(int i, Point p) {
+	/// assign value to vector
+	this->points[i] = p;
+}
+
 PointCloud PointCloud::operator+(const PointCloud& p) {
-		/// add two PointCloud
-	int tempsize = p.pointNumber + this->pointNumber;
-	PointCloud temp(tempsize);
-	for (int i = 0; i < tempsize; i++) {
-		if (i < p.pointNumber) {
-			temp.points[i] = p.points[i];
-		}
-		if (i >= p.pointNumber) {
-			temp.points[i] = this->points[i - p.pointNumber];
-		}
-	}
+	/// add two PointCloud
+	PointCloud temp;
+	temp.points.resize(this->points.size() + p.points.size());
+	copy(this->points.begin(), this->points.end(), temp.points.begin());
+	copy(p.points.begin(), p.points.end(), temp.points.begin() + this->points.size());
 	return temp;
 }
 
 PointCloud PointCloud::operator=(const PointCloud& p) {
 	/// equal operator overloading
-	this->pointNumber = p.pointNumber;
-	points = new Point[pointNumber];
-	for (int i = 0; i < p.pointNumber; i++) {
-		points[i] = p.points[i];
-	}
+	this->points = p.points;
 	return *this;
 }
