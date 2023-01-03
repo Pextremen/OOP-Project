f726b0d52d0a30f842eff8068e13199d6124bba5
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2023-01-03 21:03:09 +0300

    Update PointCloud.h
    using vector

--- OOP Project v2/OOP Project/PointCloud.h
+++ OOP Project v2/OOP Project/PointCloud.h
@@ -1,17 +1,18 @@
+///using vector
 #pragma once
-#include <list>
 #include"Point.h"
+#include <vector>
+using namespace std;
 class PointCloud {
 private:
-	//! list of points
-	list<PointCloud> points;
-	//int pointNumber;                    v2 don't have that attribute
+	vector<Point> points; /// vector of points
+	int pointNumber; /// size of vector 
 public:
 	PointCloud(int pointNumber = 0);
 	PointCloud(PointCloud&);
-//	void setpointNumber(int pointNumber); v2 don't have that attribute
-//	int getpointNumber();                 v2 don't have that attribute
-	Point* getPoints();
+	void setpointNumber(int pointNumber);
+	int getpointNumber();
+	vector<Point>& getPoints();
 	void setPoints(int, Point);
 	PointCloud operator +(const PointCloud&);
 	PointCloud  operator =(const PointCloud&);
