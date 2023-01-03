27f91ebf959149e5118b11bd0c73b979d28d13f4
armagantoros <71492330+armagantoros@users.noreply.github.com>
2023-01-03 21:34:51 +0300

    Update Transform.cpp

--- OOP Project v2/OOP Project/Transform.cpp
+++ OOP Project v2/OOP Project/Transform.cpp
@@ -1,78 +1,54 @@
 #include "Transform.h"
 Transform::Transform(){
-	for (int i = 0; i < 3; i++)
-	{
-		angles[i] = 0;
-		trans[i] = 0;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			transMatrix[i][j] = 0;
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			rotationMatrix[i][j] = 0;
-		}
-	}
+	angles << 0, 0,
+			  0, 0;
+	trans << 0, 0,
+			 0, 0;
+	transMatrix << 0, 0, 0, 0,
+				   0, 0, 0, 0,
+				   0, 0, 0, 0,
+				   0, 0, 0, 0;
+	rotationMatrix << 0, 0, 0,
+					  0, 0, 0,
+					  0, 0, 0;
 }
 
 void Transform::setAngles(double a, double b, double c)
 {
 	///Sets angles
-	angles[0] = a;
-	angles[1] = b;
-	angles[2] = c;
+	angles << a, b, c;
 }
 
-double* Transform::getAngles()
+Eigen::Vector3d* Transform::getAngles()
 {
 	///Returns angles
-	return angles;
+	return &angles;
 }
 
 void Transform::setTrans(double a, double b, double c){
-	trans[0] = a;
-	trans[1] = b;
-	trans[2] = c;
+	trans << a, b, c;
 }
 
-double* Transform::getTrans()
+Eigen::Vector3d* Transform::getTrans()
 {
-	return trans;
+	return &trans;
 }
 
 void Transform::setRotation(double ang[3]){ 
 	///Creates the rotationMatrix by performing the rotation operation
 	//alfa = angles[2] || beta = angles[1] || gama = angles[0] 
-	rotationMatrix[0][0] = cos(ang[2]) * cos(ang[1]);
-	rotationMatrix[0][1] = (cos(ang[2]) * sin(ang[1]) * sin(ang[0])) - (sin(ang[2]) * cos(ang[0]));
-	rotationMatrix[0][2] = (cos(ang[2]) * sin(ang[1])) * cos(ang[0]) + (sin(ang[2]) * sin(ang[0]));
-	rotationMatrix[1][0] = sin(ang[2]) * cos(ang[1]);
-	rotationMatrix[1][1] = (sin(ang[2]) * sin(ang[1]) * sin(ang[0])) + (cos(ang[2]) * cos(ang[0]));
-	rotationMatrix[1][2] = (sin(ang[2]) * sin(ang[1]) * cos(ang[0])) - (cos(ang[2]) * sin(ang[0]));
-	rotationMatrix[2][0] = -sin(ang[1]);
-	rotationMatrix[2][1] = cos(ang[1]) * sin(ang[0]);
-	rotationMatrix[2][2] = cos(ang[1]) * cos(ang[0]);
+	rotationMatrix << cos(ang[2]) * cos(ang[1]), (cos(ang[2]) * sin(ang[1]) * sin(ang[0])) - (sin(ang[2]) * cos(ang[0])), (cos(ang[2]) * sin(ang[1]))* cos(ang[0]) + (sin(ang[2]) * sin(ang[0])),
+		sin(ang[2])* cos(ang[1]), (sin(ang[2]) * sin(ang[1]) * sin(ang[0])) + (cos(ang[2]) * cos(ang[0])), (sin(ang[2]) * sin(ang[1]) * cos(ang[0])) - (cos(ang[2]) * sin(ang[0])),
+		-sin(ang[1]), cos(ang[1])* sin(ang[0]), cos(ang[1])* cos(ang[0]);
 
 }
-void Transform::setRotation(double rotation[3][3]) { 
+void Transform::setRotation(Eigen::Vector3d ang) { 
 	///Creates the rotationMatrix
-	rotationMatrix[0][0] = rotation[0][0];
-	rotationMatrix[0][1] = rotation[0][1];
-	rotationMatrix[0][2] = rotation[0][2];
-	rotationMatrix[1][0] = rotation[1][0];
-	rotationMatrix[1][1] = rotation[1][1];
-	rotationMatrix[1][2] = rotation[1][2];
-	rotationMatrix[2][0] = rotation[2][0];
-	rotationMatrix[2][1] = rotation[2][1];
-	rotationMatrix[2][2] = rotation[2][2];
+	rotationMatrix << cos(ang(2)) * cos(ang(1)), (cos(ang(2)) * sin(ang(1)) * sin(ang(0)) - (sin(ang(2)) * cos(ang(0)))), (cos(ang(2)) * sin(ang(1))* cos(ang(0)) + (sin(ang(2)) * sin(ang(0)))),
+		sin(ang(2))* cos(ang(1)), (sin(ang(2)) * sin(ang(1)) * sin(ang(0))) + (cos(ang(2)) * cos(ang(0))), (sin(ang(2)) * sin(ang(1)) * cos(ang(0))) - (cos(ang(2)) * sin(ang(0))),
+		-sin(ang(1)), cos(ang(1))* sin(ang(0)), cos(ang(1))* cos(ang(0));
 }
-void Transform::setTranslation()
+void Transform::setTranslation(Eigen::Vector3d tr)
 {
 	//transMatrix is a combination of rotationMatrix and trans
 	//R R R T 
@@ -81,17 +57,17 @@ void Transform::setTranslation()
 	//0 0 0 1
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
-			transMatrix[i][j] = rotationMatrix[i][j];
+			transMatrix(i,j) = rotationMatrix(i,j);
 		}
 	}
 	for (int i = 0; i < 3; i++) {
-		transMatrix[i][3] = trans[i];
+		transMatrix(i,3) = trans(i);
 	}
 	// aktarma 3 son satir
-	transMatrix[3][0] = 0;
-	transMatrix[3][1] = 0;
-	transMatrix[3][2] = 0;
-	transMatrix[3][3] = 1;
+	transMatrix(3, 0) = 0;
+	transMatrix(3, 1) = 0;
+	transMatrix(3, 2) = 0;
+	transMatrix(3, 3) = 1;
 }
 
 Point Transform::doTransform(Point p){
@@ -103,7 +79,7 @@ Point Transform::doTransform(Point p){
 	//matris çarpýmý
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			newPoint[i] += (transMatrix[i][j] * oldPoint[j]);
+			newPoint[i] += (transMatrix(i,j) * oldPoint[j]);
 		}
 	}
 	//The new matrix obtained will be transferred to the new point
