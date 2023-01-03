a3232f9240c8c568ee80c3aff2131dfd54f79712
canercoskuun <101167021+canercoskuun@users.noreply.github.com>
2023-01-03 22:03:19 +0300

    Update Transform.h

--- OOP Project v2/OOP Project/Transform.h
+++ OOP Project v2/OOP Project/Transform.h
@@ -1,11 +1,12 @@
 #pragma once
 #include<Eigen/Dense>
-class Transform{
+#include "PointCloud.h"
+class Transform {
 private:
 	Eigen::Vector3d angles;  /*!<Holds three angle values, these angles will be used in rotation.*/
-	Eigen::Vector3d trans; /*!<Holds the distances between the origins of two coordinate axes.*/  
-	Eigen::Matrix4d transMatrix; /*!<Creates the transformation matrix, transMatrix is a combination of rotationMatrix and trans*/ 
-	Eigen::Matrix3d rotationMatrix; /*!<Creates the rotation matrix*/ 
+	Eigen::Vector3d trans; /*!<Holds the distances between the origins of two coordinate axes.*/
+	Eigen::Matrix4d transMatrix; /*!<Creates the transformation matrix, transMatrix is a combination of rotationMatrix and trans*/
+	Eigen::Matrix3d rotationMatrix; /*!<Creates the rotation matrix*/
 
 public:
 	//! Constructor of Transform Class
@@ -16,6 +17,7 @@ public:
 	Eigen::Vector3d* getTrans();
 	void setRotation(double angles[]);
 	void setRotation(Eigen::Vector3d);
+	void setRotation(double rotation[3][3]);
 	void setTranslation(Eigen::Vector3d);
 	Point doTransform(Point p);
 	PointCloud doTransform(PointCloud& pc);
