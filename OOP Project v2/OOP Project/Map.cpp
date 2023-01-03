54276899f9f4c0ed29ea776bbdffd192a8d9aa10
LAST <94117509+Pextremen@users.noreply.github.com>
2023-01-03 23:21:47 +0300

    Revert "map update"
    This reverts commit e0c5101a7ef4d9f565e95eb24d9aa3f0a9c0cc41.

---
+++ OOP Project v2/OOP Project/Map.cpp
@@ -0,0 +1,29 @@
+#include "Map.h"
+Map::Map()
+{
+
+}
+void Map::setMap()
+{
+
+}
+Map::getMap()
+{
+
+}
+void Map::insertPointCloud(PointCloud& pc)
+{
+
+}
+void Map::insertPoint(Point& p)
+{
+
+}
+bool Map::loadMap(fileName)
+{
+
+}
+bool Map::saveMap(fileName)
+{
+
+}
--- OOP Project v2/OOP Project/Map.h
+++ OOP Project v2/OOP Project/Map.h
@@ -5,6 +5,6 @@ public:
 	Map();
 	virtual void insertPointCloud(PointCloud& pc) = 0;
 	virtual void insertPoint(Point& p) = 0;
-	virtual bool loadMap(string fileName) = 0;
-	virtual bool saveMap(string fileName) = 0;
+	virtual bool loadMap(fileName) = 0;
+	virtual bool saveMap(fileName) = 0;
 };
