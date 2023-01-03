cd7578a1ce10457527920b1148c54e47c686b4b7
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2023-01-03 22:25:35 +0300

    Update MapperInterface.cpp
    some part of MapperInterface

--- OOP Project v2/OOP Project/MapperInterface.cpp
+++ OOP Project v2/OOP Project/MapperInterface.cpp
@@ -1,29 +1,48 @@
 #include "MapperInterface.h"
+
 MapperInterface::MapperInterface()
 {
-
+   
 }
-void MapperInterface::addGenerator(PointCloudGenerator*)
+
+void MapperInterface::addGenerator(PointCloudGenerator* generator)
+{
+   
+    generators.push_back(generator);
+}
+
+void MapperInterface::setRecorder(PointCloudRecorder* recorder)
 {
 
+    this->recorder = recorder;
 }
-void MapperInterface::setRecorder(PointCloudRecorder*)
-{
 
-}
 bool MapperInterface::generate()
 {
+    
+    for (auto generator : generators)
+    {
+        auto cloud = generator->captureFor();
+        pointCloud.push_back(cloud);
+    }
 
+    return !pointCloud.empty();
 }
+
 bool MapperInterface::recordPointCloud()
 {
-
+    if (recorder)
+        return recorder->record(pointCloud);
+    return false;
 }
+
 bool MapperInterface::recordMap()
 {
-
+   // mapi kaydet
 }
+
 bool MapperInterface::insertMap()
 {
-
+    // mape nokta bulutlarını ekle
+    //nesnenin insert fonksiyonunu çağır
 }
