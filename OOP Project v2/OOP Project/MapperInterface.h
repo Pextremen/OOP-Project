f157884da6482aa87a2c7ceea07760d79b43c0dd
Kadir Sonmezisik <101130181+akadirson@users.noreply.github.com>
2023-01-04 21:48:38 +0300

    Update MapperInterface.h

--- OOP Project v2/OOP Project/MapperInterface.h
+++ OOP Project v2/OOP Project/MapperInterface.h
@@ -3,20 +3,22 @@
 #include"PointCloudGenerator.h"
 #include"PointCloud.h"
 #include"Map.h"
+#include "3DGridMap.h"
 #include"PointCloudRecorder.h"
+class Map;
 class MapperInterface {
 private:
-    PointCloud pointCloud;
-    PointCloud patch;
-    vector<PointCloudGenerator*> generators;
-    PointCloudRecorder* recorder;
-    int *map;
+	PointCloud pointCloud; //final cloud
+	PointCloud patch;
+	vector<PointCloudGenerator*> generators; //
+	PointCloudRecorder* recorder; //pc yazma için
+    Map* map; //map yazma için
 public:
-    MapperInterface(string);
-    void addGenerator(PointCloudGenerator*);
-    void setRecorder(PointCloudRecorder*);
-    bool generate();
-    bool recordPointCloud();
-    bool recordMap();
-    bool insertMap();
+	MapperInterface(string);
+	void addGenerator(PointCloudGenerator*);
+	void setRecorder(PointCloudRecorder*);
+	bool generate();
+	bool recordPointCloud();
+	bool recordMap();
+	bool insertMap();
 };
