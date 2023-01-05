d99a9035533e90bec8148ef21e61ee2da3bbda3e
canercoskuun <101167021+canercoskuun@users.noreply.github.com>
2023-01-05 23:01:02 +0300

    Update Map.h

--- OOP Project v2/OOP Project/Map.h
+++ OOP Project v2/OOP Project/Map.h
@@ -2,8 +2,8 @@
 #include "PointCloud.h"
 class Map {
 public:
-	virtual void insertPointCloud(PointCloud& pc) = 0;
-	virtual void insertPoint(Point& p) = 0;
-	virtual bool loadMap(string) = 0;
-	virtual bool saveMap(string) = 0;
+	virtual void insertPointCloud(PointCloud& pc) = 0;//pure virtual function
+	virtual void insertPoint(Point& p) = 0;//pure virtual function
+	virtual bool loadMap(string) = 0;//pure virtual function
+	virtual bool saveMap(string) = 0;//pure virtual function
 };
