681f76c84ae944eaf7d821b32212fe15af275b13
LAST <94117509+Pextremen@users.noreply.github.com>
2023-01-05 23:34:37 +0300

    Update PointCloudGenerator.h

--- OOP Project v2/OOP Project/PointCloudGenerator.h
+++ OOP Project v2/OOP Project/PointCloudGenerator.h
@@ -4,8 +4,11 @@
 
 class PointCloudGenerator
 {
+	//! Object of Transform
 	Transform transform;	
 public:
-	virtual PointCloud capture() = 0;//pure virtual function
-	virtual PointCloud captureFor() = 0;//pure virtual function
+	//! When the capture function is called, it reads the points and assigns them to the created point cloud object. Returns the point cloud. This process is a simulation of the camera. Points are taken from a file rather than a camera.
+	virtual PointCloud capture() = 0;//Bu fonksiyon pure virtual fonksiyondur.
+	//! First of all, it will do the same with the capture function; however, the point cloud will be returned after it has been transformed with the transform object of which it is a member.
+	virtual PointCloud captureFor() = 0;//Bu fonksiyon pure virtual fonksiyondur.
 };
