c1443637946d6cce35a1edeabaf249270c3b3f75
Ayşe Ayhan <101136142+AyseAyhan@users.noreply.github.com>
2023-01-05 23:13:27 +0300

    Update MapperInterface.h

--- OOP Project v2/OOP Project/MapperInterface.h
+++ OOP Project v2/OOP Project/MapperInterface.h
@@ -8,11 +8,11 @@
 
 class MapperInterface {
 private:
-	PointCloud pointCloud; ///final cloud
+	PointCloud pointCloud;  /*!< final cloud */  
 	PointCloud patch;
-	vector<PointCloudGenerator*> generators; ///vector for generators
+	vector<PointCloudGenerator*> generators; /*!<vector for generators */      
 	PointCloudRecorder* recorder;
-    Map* map; /// to write-save map
+    Map* map; /*!< to write-save map*/      
 public:
 	MapperInterface(Map*, PointCloudRecorder*, vector<PointCloudGenerator*>);
 	void addGenerator(PointCloudGenerator*);
