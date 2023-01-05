1d098ff0c1c225619bda77d4fa85db389acf9475
Kadir Sonmezisik <101130181+akadirson@users.noreply.github.com>
2023-01-05 22:00:32 +0300

    the end

--- OOP Project v2/OOP Project/PointCloudGenerator.h
+++ OOP Project v2/OOP Project/PointCloudGenerator.h
@@ -1,10 +1,14 @@
 #pragma once
-#include "Transform.h"
-#include "PointCloud.h"
-class PointCloudGenerator {
-	Transform transform;
+#include"Transform.h"
+#include"PointCloud.h"
+
+class PointCloudGenerator
+{
+	Transform transform;	
 public:
+
 	PointCloudGenerator();
-	virtual PointCloud capture() = 0;
-	virtual PointCloud captureFor() = 0;
+	virtual PointCloud capture() = 0;//Bu fonksiyon pure virtual fonksiyondur.
+	virtual PointCloud captureFor() = 0;//Bu fonksiyon pure virtual fonksiyondur.
+	~PointCloudGenerator();
 };
