82957d50b8a2d843f2a3bf3efb126dff4b626cb6
Kadir Sonmezisik <101130181+akadirson@users.noreply.github.com>
2023-01-05 21:57:42 +0300

    the end

--- OOP Project v2/OOP Project/MapperInterface.cpp
+++ OOP Project v2/OOP Project/MapperInterface.cpp
@@ -1,9 +1,7 @@
 #include "MapperInterface.h"
 
-MapperInterface::MapperInterface(string fileName) {
-    map = new _3DGridMap(0, 0);
-    recorder->setfileName(fileName);
-}
+MapperInterface::MapperInterface(Map* map, PointCloudRecorder* rec, vector<PointCloudGenerator*> arr):map(map),
+recorder(rec), generators(arr){}
 
 void MapperInterface::addGenerator(PointCloudGenerator* generator)
 {
@@ -14,12 +12,22 @@ void MapperInterface::setRecorder(PointCloudRecorder* recorder)
 {
     this->recorder = recorder;
 }
-
-bool MapperInterface::generate(){
-    for (int i = 0; i < generators.size(); i++) {
-        pointCloud = pointCloud + generators[i]->captureFor();
+bool MapperInterface::generate()
+{
+    // PointCloudGenerator nesnesi yok ya da eklenmemis.
+    if (generators.empty())
+    {
+        return false;
     }
-    return 1;
+    else
+    {
+        for (int i = 0; i < generators.size(); i++)
+        {
+            patch = generators[i]->captureFor();
+            pointCloud = pointCloud + patch;
+        }
+        return true;
+    }
 }
 
 bool MapperInterface::recordPointCloud()
@@ -32,7 +40,7 @@ bool MapperInterface::insertMap() {
    map->insertPointCloud(pointCloud);
    return 1;
 }
-bool MapperInterface::recordMap(){
-   map->saveMap("map.txt");    // mapi kaydet
+bool MapperInterface::recordMap() {
+   map->saveMap("map.txt"); //map'i kaydet
    return 1;
 }
