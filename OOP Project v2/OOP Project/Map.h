6dab212272827deeaea4e7eb7fd142b1f4c05f14
LAST <94117509+Pextremen@users.noreply.github.com>
2023-01-05 23:49:51 +0300

    Update Map.h

--- OOP Project v2/OOP Project/Map.h
+++ OOP Project v2/OOP Project/Map.h
@@ -2,8 +2,12 @@
 #include "PointCloud.h"
 class Map {
 public:
-	virtual void insertPointCloud(PointCloud& pc) = 0;//pure virtual function
-	virtual void insertPoint(Point& p) = 0;//pure virtual function
-	virtual bool loadMap(string) = 0;//pure virtual function
-	virtual bool saveMap(string) = 0;//pure virtual function
+	//! pure virtual function of Map
+	virtual void insertPointCloud(PointCloud& pc) = 0;
+	//! pure virtual function of Map
+	virtual void insertPoint(Point& p) = 0;
+	//! pure virtual function of Map
+	virtual bool loadMap(string) = 0;
+	//! pure virtual function of Map
+	virtual bool saveMap(string) = 0;
 };
